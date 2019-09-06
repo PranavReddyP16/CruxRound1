79ebf786a0216d51aaeb9971a07a352f635ccf50
Pranav Reddy <f20180238@hyderabad.bits-pilani.ac.in>
2019-09-06 21:55:52 +0530

    initial uploading of the entire code

---
+++ Assets/Scripts/MovementManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementManager : MonoBehaviour
+{
+	private UnityEngine.AI.NavMeshAgent NMA;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+		NMA = GetComponent<UnityEngine.AI.NavMeshAgent>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+		Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+
+		RaycastHit hit;
+
+		if(Input.GetMouseButtonDown(0))
+		{
+			if(Physics.Raycast(ray, out hit, 100))
+			{
+				NMA.destination = hit.point;
+			}
+
+			else
+			{
+				Debug.Log("Please click only on the plane");
+			}
+		}
+    }
+}
